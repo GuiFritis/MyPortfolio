0431f62dfa0238b5647e4305afff5a4b83a97f9f
Guilherme Carvalho Fritis <Guilhermecfritis@gmail.com>
2024-07-11 15:51:41 -0300

    work in progress
    add dropdown for languages, and puts the dark mode switch on the footer.

--- js/script.js
+++ js/script.js
@@ -1,45 +1,58 @@
-var cur_section = '';
+const url_params = new URLSearchParams(window.location.search);
+var cur_section = url_params.has('section')?url_params.get('section'):'./content/about_me.html';
+var mode = url_params.has('mode')?url_params.has('mode'):
+    (window.matchMedia('(prefers-color-scheme: light)').matches?'light':'dark');
 
 window.addEventListener('load', (event) => {
-    if(window.matchMedia('(prefers-color-scheme: dark)').matches)
+    if(mode == 'light')
     {
         document.getElementById('theme-switch-checkbox').setAttribute('checked', true);
-        document.body.classList.add('dark');
-    }
-    else
-    {        
         document.body.classList.add('light');
     }
+    else
+    {      
+        document.body.classList.add('dark');
+    }
     document.getElementById('theme-switch-checkbox').addEventListener("change", switchLightMode);
-
-    openSection('./content/about_me.html');
+    dropdowns = document.getElementsByClassName('dropdown-link');
+    for (let i = 0; i < dropdowns.length; i++) {
+        const element = dropdowns[i];
+        element.addEventListener('click', function(ev){
+            element.parentElement.classList.toggle("open");
+        });
+    }
+    openSection(cur_section);
 });
 
 function switchLightMode(ev)
 {    
     if(ev.target.checked)
     {
-        document.body.classList.add('dark');
-        document.body.classList.remove('light');
-    }
-    else
-    {
+        mode = 'light';
         document.body.classList.add('light');
         document.body.classList.remove('dark');
     }
+    else
+    {
+        mode = 'dark';
+        document.body.classList.add('dark');
+        document.body.classList.remove('light');
+    }
 }
 
-function openSection(link)
+function openSection(link, a = null)
 {
-    if(cur_section != link)
+    if(a)
     {
-        cur_section = link;
-        const request = new XMLHttpRequest();
-        request.onload = function() {
-            document.getElementById("page-content").innerHTML = this.responseText;
-        }
-        request.onerror = function(e){console.log(e)}
-        request.open("GET", link, false);
-        request.send();
+        document.querySelector(".nav-item.active").classList.remove("active");
+        a.classList.add("active");
     }
+    cur_section = link;
+    const request = new XMLHttpRequest();
+    request.onload = function() {
+        document.getElementById("page-content").innerHTML = this.responseText;
+    }
+    request.onerror = function(e){console.log(e)}
+    request.open("GET", link, false);
+    request.send();
 }
